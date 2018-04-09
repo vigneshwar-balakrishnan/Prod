4903f9993f52b6189cfdb03b8ca0b0cbee13a939
vigneshwar-balakrishnan <vigneshwar.balakrishnan@gmail.com>
2018-04-09 17:10:56 +0530

    password input added

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,18 +3,27 @@ import { View, TextInput } from 'react-native';
 import { Button , Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
-    state = { text: ''};
+    state = { email: '', password: ''};
     render () {
         return (
             <Card>
                 <CardSection>
                     <Input 
+                    placeholder="MotherTeresa@gmail.com"
                     label="Email"
-                    value={this.state.text}
-                    onChangeText={text => this.setState({ text })}
+                    value={this.state.email}
+                    onChangeText={text => this.setState({ email: text })}
                     />
                 </CardSection>
-                <CardSection />
+                <CardSection>
+                <Input 
+                    secureTextEntry
+                    placeholder="password"
+                    label="Password"
+                    value={this.state.password}
+                    onChangeText={text => this.setState({ password: text })}
+                    />
+                </CardSection>
 
                 <CardSection>
                     <Button>
