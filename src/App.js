3350b1c44b90afb50c38266306d5df295aff8b7d
vigneshwar-balakrishnan <vigneshwar.balakrishnan@gmail.com>
2018-04-09 14:31:13 +0530

    text input given state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import  LoginForm  from './components/LoginForm'
 
 class App extends Component{
     componentWillMount(){
@@ -18,9 +19,8 @@ class App extends Component{
         return(
             <View>
                 <Header headerText="Auth" />
-                <Text>lalala</Text>
-            </View>
-            
+                <LoginForm />
+            </View>            
         );
     }
 }
